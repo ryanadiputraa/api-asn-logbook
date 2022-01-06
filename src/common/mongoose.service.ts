dbccc68e6ae782ee71ae1eb84ed4765fe3a5b8b0
Ryan Adi Putra <ryannadiputraa@gmail.com>
2022-01-06 11:08:47 +0800

    connect to mongo atlas

--- src/common/mongoose.service.ts
+++ src/common/mongoose.service.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 import { logger } from "./logger";
 
-const mongoHost = Boolean(process.env.DEBUG) ? "localhost" : "mongo";
+const mongoHost = Boolean(process.env.LOCAL_DB) ? "localhost" : "mongo";
+const localMongoURI = `mongodb://${mongoHost}:27017/laporanasn-db`;
+const cloudMongoURI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@laporan-asn-shard-00-00.ulnkp.mongodb.net:27017,laporan-asn-shard-00-01.ulnkp.mongodb.net:27017,laporan-asn-shard-00-02.ulnkp.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-uf99ze-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 class MongooseService {
   private count = 0;
@@ -10,7 +12,6 @@ class MongooseService {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 20000,
-    useFindModify: false,
   };
 
   constructor() {
@@ -25,13 +26,15 @@ class MongooseService {
     logger.info("Attempting MongoDB connection (will retry if needed)");
     mongoose
       .connect(
-        `mongodb://${mongoHost}:27017/laporanasn-db`,
+        process.env.STAGE === "prod" ? cloudMongoURI : localMongoURI,
         this.mongooseOptions
       )
       .then(() => {
         logger.info("MongoDB connected");
       })
       .catch((error) => {
+        console.log(error);
+
         const retrySeconds = 5;
         logger.error(
           `Fail to connect to MongoDB (will retry ${++this
